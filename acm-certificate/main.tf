ac40d174ea883a0ca330486ac4e2290ab7b66e92
Garegin Tamrazyan <37587344+garegintamrazyan07@users.noreply.github.com>
2021-09-10 18:09:53 +0400

    added vars and resources for cloudtrail (#7)
    Co-authored-by: Garegin <gtamrazyan@provectus.com>

--- acm-certificate/main.tf
+++ acm-certificate/main.tf
@@ -17,6 +17,114 @@ provider "aws" {
 }
 
 
+
+resource "aws_cloudwatch_log_group" "acm_cloudtrail_logs" {
+  count = var.enable_cloudtrail_logging ? 1 : 0
+  name  = "acm-cloudtrail-logs-${var.domain_name}"
+}
+
+data "aws_iam_policy_document" "cloudtrail-assume-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+  }
+}
+
+
+resource "random_string" "role_suffix" {
+  length  = 5
+  special = false
+}
+
+
+resource "aws_s3_bucket" "acm_cloudtrail_logs" {
+
+  count  = var.enable_cloudtrail_logging ? 1 : 0
+  bucket = "acm-cloudtrail-logs-${var.domain_name}"
+  tags   = var.tags
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "acm-cloudtrail-logs-${var.domain_name}"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "acm-cloudtrail-logs-${var.domain_name}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
+
+
+
+resource "aws_iam_role" "cloudtrail_to_cloudwatch" {
+  count = var.enable_cloudtrail_logging ? 1 : 0
+  name  = "CloudWatchWriteForCloudTrail-${random_string.role_suffix.result}"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = data.aws_iam_policy_document.cloudtrail-assume-role-policy.json
+  inline_policy {
+    name = "cloudwatch_write_permissions"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = [
+            "cloudwatch:PutMetricData",
+            "logs:PutLogEvents",
+            "logs:DescribeLogStreams",
+            "logs:DescribeLogGroups",
+            "logs:CreateLogStream",
+            "logs:CreateLogGroup"
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+}
+
+
+resource "aws_cloudtrail" "acm" {
+  count                         = var.enable_cloudtrail_logging ? 1 : 0
+  name                          = "acm-trail"
+  s3_bucket_name                = aws_s3_bucket.acm_cloudtrail_logs[0].id
+  s3_key_prefix                 = "trail"
+  include_global_service_events = false
+
+
+  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.acm_cloudtrail_logs[0].arn}:*"
+  cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail_to_cloudwatch[0].arn
+}
+
+
 # normal acm certificate
 module "acm_certificate" {
   source  = "terraform-aws-modules/acm/aws"
